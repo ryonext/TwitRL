a46dbb4131ae9fc642344911a923a062cd331663
ryonext <ryo.next@gmail.com>
2012-01-31 22:03:33 +0900

    default rails3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitRL::Application.config.secret_token = '4a2cc16b7a2d1b86f8636f01d22ab667d9849f8d8e5eb7a380eddd812481aaa7477cf6d4fae15fa56a28180cffc2465665c9ded6abbcff4077e7bd36abbf40a1'
