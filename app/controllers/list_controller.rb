0dc2f654fd928bfa011746a84021d9b0d35e9fcb
ryonext <ryo.next@gmail.com>
2012-02-02 22:16:20 +0900

    list#index

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,17 @@
+class ListController < ApplicationController
+  def index
+  end
+
+  def select
+    #さて。
+    #OAuthする
+    
+    #フォローした人を取得する
+    
+    #表示する
+  end
+
+  def create
+  end
+
+end
