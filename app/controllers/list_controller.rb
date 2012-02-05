42770f5946f98a391ccad831ae6ca405b3c2348c
ryonext <ryo.next@gmail.com>
2012-02-05 21:50:20 +0900

    list_controller作り中。

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -6,8 +6,18 @@ class ListController < ApplicationController
     #さて。
     #OAuthする
     
+    auth = request.env["omniauth.auth"]
+    p auth
+    #p auth.credentials.secret
+    #p auth.credentials.token
+    Twitter.configure do |config|
+      config.consumer_key =  'cNa4bLkDOTkmHgCGMgybSw'
+      config.consumer_secret = 'bCQbpcANe5DjZoi8d04lZuEdvh8TqRD7voSt96ximS4'
+      config.oauth_token = auth['credentials']['token']
+      config.oauth_token_secret = auth['credentials']['secret']
+    end
     #フォローした人を取得する
-    
+    @followers = Twitter.followers
     #表示する
   end
 
