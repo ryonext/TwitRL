60b2f1e4d546ceadea0db4913dffbff103258db9
ryonext <ryo.next@gmail.com>
2012-06-01 21:12:58 +0900

    [add function]complete design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+#encoding: utf-8
 module ApplicationHelper
+  def is_active(i)
+    return 'active' if is_active_page(i)
+  end
+  def is_active_page(i)
+    case i
+    when 0
+      return params[:action] == 'index'
+    when 1
+      return params[:action] == 'select'
+    when 2
+      return params[:action] == 'create'
+    end
+  end
 end
